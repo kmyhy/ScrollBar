33dc2d15cbcc75ec17b1bc04a1c3c25d5115d511
kmyhy <MY_FANGXIAN@example.com>
2017-03-22 17:24:35 +0800

    Initial Commit

---
+++ ScrollBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollBar
+//
+//  Created by qq on 2017/3/22.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
